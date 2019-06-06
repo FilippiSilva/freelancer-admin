046cfbbf84de7916c2d58d38833666576d123967
Filippi <filippiusk@gmail.com>
2019-06-05 23:20:33 -0300

    ADD: criado projeto com vue e add o admin template

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import "@/assets/scss/main.scss";
+
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .home
+    img(alt='Vue logo', src='../assets/logo.png')
+    b-alert(show) Default Alert
+    hello-world(msg='Welcome to Your Vue.js App')
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
